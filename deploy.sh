2069ce9c4350350c9268887fb4a5e21d91243499
gaolaoge <gaolaoge@sina.cn>
2023-10-06 12:43:31 +0800

    修正 deploy 流程

--- deploy.sh
+++ deploy.sh
@@ -30,7 +30,7 @@ echo 'gaolaogui.cc' >CNAME
 
 git init
 git add .
-git commit -m $commit_message
+git commit -m "$commit_message"
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
